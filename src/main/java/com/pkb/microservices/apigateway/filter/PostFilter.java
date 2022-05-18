40579e669e7e7c5e8b5c80c2c767852906fb4336
pburnwal661 <pburnwal661@gmail.com>
2022-05-18 23:00:50 +0530

    initial commit

---
+++ src/main/java/com/pkb/microservices/apigateway/filter/PostFilter.java
@@ -0,0 +1,43 @@
+package com.pkb.microservices.apigateway.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class PostFilter extends ZuulFilter {
+	
+	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		HttpServletResponse response = ctx.getResponse();
+		log.info("PostFilter : "+ String.format("Response's context type for redirected uri %s is %s", request.getRequestURI().toString(),response.getStatus()));
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+}
